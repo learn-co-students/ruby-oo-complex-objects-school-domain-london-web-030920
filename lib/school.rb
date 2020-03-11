fd65dc1a902d7fec1739f7dcf78ba4b67633d2c7
Juhagit <juha.ea.penttila@gmail.com>
2020-03-11 23:14:11 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,41 @@
-# code here!
+require 'pry'
+class School
+
+    attr_accessor :roster 
+
+def initialize(name)
+    @name = name
+    @roster = {}     
+end
+def add_student(student, grade)
+   if @roster[grade]
+    @roster[grade] << student
+   else
+    @roster[grade] = []
+    @roster[grade] << student
+    end
+end 
+
+def grade(grade)
+    @roster[grade]
+
+    # binding.pry
+    end
+def sort
+
+     @roster.each do |key, value|
+     value.sort!
+    # @roster.sort.to_h
+     end 
+
+    
+    
+
+
+
+# binding.pry
+
+end
+
+
+end 
